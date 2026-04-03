cdb395b6fa6d189fc46098f1447de3cc05b54827
Leon Xia <253962588+dogitect@users.noreply.github.com>
2026-04-03 14:24:34 +0800

    feat(environment): create environment configuration file

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+// Copyright 2025 Leon Xia. MIT License.
+
+export const environment = {
+  production: false,
+  useCloudflareImages: false,
+  siteUrl: 'https://vellum.dogitect.io',
+  cloudflareCdnUrl: 'https://assets.dogitect.io/projects/vellum',
+  analyticsId: 'G-BHNE8HRTZF',
+};
