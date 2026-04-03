36ab724a95e865b76e4e9ec1ab2d6705c791396c
Leon Xia <253962588+dogitect@users.noreply.github.com>
2026-04-03 14:24:30 +0800

    feat(environment): add production environment configuration

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+// Copyright 2025 Leon Xia. MIT License.
+
+export const environment = {
+  production: true,
+  useCloudflareImages: true,
+  siteUrl: 'https://vellum.dogitect.io',
+  cloudflareCdnUrl: 'https://assets.dogitect.io/projects/vellum',
+  analyticsId: 'G-BHNE8HRTZF',
+};
