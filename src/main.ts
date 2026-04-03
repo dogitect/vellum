631af2d609e8417a06f76870cfa28a83e64ed6a9
Leon Xia <253962588+dogitect@users.noreply.github.com>
2026-04-03 14:23:26 +0800

    feat(main): add main application bootstrap file with initial setup

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+// Copyright 2025 Leon Xia. MIT License.
+
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app';
+
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
