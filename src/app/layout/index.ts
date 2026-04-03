1236b771c73813014845e139019a21114e84e982
Leon Xia <253962588+dogitect@users.noreply.github.com>
2026-04-03 14:24:15 +0800

    feat(layout): create index file to export layout components

---
+++ src/app/layout/index.ts
@@ -0,0 +1,5 @@
+// Copyright 2025 Leon Xia. MIT License.
+
+export { NavigationComponent } from './navigation/navigation';
+export { HeroBackgroundComponent } from './hero-background/hero-background';
+export { FooterComponent } from './footer/footer';
